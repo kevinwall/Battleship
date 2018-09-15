688f8f217ac8031923f1004a7dfeafbb3bc718cc
Kevin Wallacy <kevinwall@ufrn.edu.br>
2018-09-15 19:09:51 -0300

    Remoção de arquivos ultrapassados e modificação do README.md

--- includes/armada.h
+++
@@ -1,8 +0,0 @@
-#include "marker.h"
-// This is armada builder 
-void horizontal_battleship( int x, int y );
-void vertical_battleship( int x, int y );
-void horizontal_cruiser( int x, int y );
-void vertical_cruiser( int x, int y );
-void horizontal_cruiser( int x, int y );
-void vertical_cruiser( int x, int y );
